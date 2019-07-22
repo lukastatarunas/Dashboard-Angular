ab9f6cf5079cf0272087d90639430f95cf1fefa9
Lukas Tatarunas <lukas@LT-LIT-SC-0022.internal.visma.com>
2019-07-22 14:01:39 +0300

    Added aside bar

---
+++ src/app/app.module.ts
@@ -0,0 +1,20 @@
+import { BrowserModule } from '@angular/platform-browser';
+import { NgModule } from '@angular/core';
+
+import { AppRoutingModule } from './app-routing.module';
+import { AppComponent } from './app.component';
+import { AsideComponent } from './components/aside/aside.component';
+
+@NgModule({
+  declarations: [
+    AppComponent,
+    AsideComponent
+  ],
+  imports: [
+    BrowserModule,
+    AppRoutingModule
+  ],
+  providers: [],
+  bootstrap: [AppComponent]
+})
+export class AppModule { }
